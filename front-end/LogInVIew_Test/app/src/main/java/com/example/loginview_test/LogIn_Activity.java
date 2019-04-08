c3850dbf4fab44845a1e774d54d59cc6dbec4d14
Dudebla <20172131134@m.scnu.edu.cn>
2019-04-08 21:04:18 +0800

    上传登录界面、注册界面，已实现基本检测

---
+++ front-end/LogInVIew_Test/app/src/main/java/com/example/loginview_test/LogIn_Activity.java
@@ -0,0 +1,137 @@
+//  LogIn_Activity.java   ----    login_activity.xml
+package com.example.loginview_test;
+
+import android.content.Intent;
+import android.service.autofill.RegexValidator;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LogIn_Activity extends AppCompatActivity {
+
+    private EditText username_editText, password_editText;
+    private TextView signin_textView, forgetPW_textView;
+    private Button login_button;
+
+    //  statement of username and password
+    private static final int EMPTY_MSG = 0;
+    private static final int USERNAME_OUT_OF_RANGE = 1;
+    private static final int PASSWORD_OUT_OF_RANGE = 2;
+    private static final int USERNAME_INCLUDE_ILLEGAL_CHAR = 3;
+    private static final int PASSWORD_INCLUDE_ILLEGAL_CHAR = 4;
+    private static final int MSG_LEGAL = 5;
+    private static final int USERNAME_NOT_FOUND = 6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        username_editText = (EditText)findViewById(R.id.login_username_editText);
+        password_editText = (EditText)findViewById(R.id.login_password_editText);
+        login_button = (Button)findViewById(R.id.login_login_button);
+        signin_textView = (TextView)findViewById(R.id.login_signIn_textView);
+        forgetPW_textView = (TextView)findViewById(R.id.login_forgetPW_textView);
+
+        setOnClickListenerOfWidget();
+
+
+    }
+
+
+    //  set onClickListener of login_button, signin_textView
+    private void setOnClickListenerOfWidget(){
+
+        //  set onClickListener of button, check the msg
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int statement = getLogInMsg();
+                String tipMsg = "";
+                switch (statement){
+                    case EMPTY_MSG:
+                        tipMsg = "用户名和密码不能为空";
+                        break;
+                    case USERNAME_INCLUDE_ILLEGAL_CHAR:
+                    case USERNAME_OUT_OF_RANGE:
+                        tipMsg = "用户名长度为2~16，只包含汉子、数字、字母和下划线_";
+                        break;
+                    case PASSWORD_INCLUDE_ILLEGAL_CHAR:
+                    case  PASSWORD_OUT_OF_RANGE:
+                        tipMsg = "密码长度为8～16，只包含数字、字母、特殊符号_@#";
+                        break;
+                    default:
+                }
+                if(!"".equals(statement)) {
+                    Toast.makeText(v.getContext(), tipMsg, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //  set onClickListener of signin_textView, link to SignIn_Activity
+        signin_textView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn_Activity.this, SignIn_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    //  getLogInMsg:int
+    //  check the msg of username and password, return statement
+    private int getLogInMsg(){
+        int statement;
+
+        String username = username_editText.getText().toString();
+        String password = password_editText.getText().toString();
+
+        if("".equals(username) || "".equals(password)){
+            statement = EMPTY_MSG;
+        }else if(username.length()>15 || username.length()<2) {
+            statement = USERNAME_OUT_OF_RANGE;
+        }else if(password.length()>16 || password.length()<8){
+            statement = PASSWORD_OUT_OF_RANGE;
+        }else{
+            return checkMsgChar(username, password);
+        }
+        return statement;
+    }
+
+    private int checkMsgChar(String username, String password){
+        //  username只含有汉字、数字、字母、下划线不能以下划线开头和结尾
+        //  password只含有字母、数字、_@#
+        String regOfUsername = "^[(?!_)(?!.*?_$)[a-zA-Z0-9_\\u4e00-\\u9fa5]]{2,16}$";
+        String regOfPassword = "^[a-zA-Z0-9_@#]{8,16}$";
+
+        Pattern pattern = Pattern.compile(regOfUsername);
+        Matcher matcher = pattern.matcher(username);
+        if(matcher.find()){
+            pattern = Pattern.compile(regOfPassword);
+            matcher = pattern.matcher(password);
+            if(matcher.find()){
+                return MSG_LEGAL;
+            }
+            else {
+                return PASSWORD_INCLUDE_ILLEGAL_CHAR;
+            }
+        }else{
+            return USERNAME_INCLUDE_ILLEGAL_CHAR;
+        }
+    }
+
+
+}
---
+++ front-end/LogInVIew_Test/app/src/main/java/com/example/loginview_test/SignIn_Activity.java
@@ -0,0 +1,134 @@
+//  SignIn_Activity.java ----signin_layout.xml
+
+//  customerUserType | sellerUserType
+//  username, tellNumber, password, passwordRV: for editView of signin_layout, for sign in msg
+//  tip_***: textView of signin_layout, visible while *** on focus
+
+package com.example.loginview_test;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SignIn_Activity extends AppCompatActivity {
+
+
+    private RadioButton customerUserType;
+    private RadioButton sellerUserType;
+    private EditText username_editText;
+    private EditText tellNumber_editText;
+    private EditText password_editText;
+    private EditText passwordRV_editText;
+    private TextView tip_username;
+    private TextView tip_password;
+    private TextView tip_passwordRV;
+    private TextView hasId_textView;
+    private Button signIn_button;
+    private Boolean msgIsLegal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_layout);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        customerUserType = (RadioButton)findViewById(R.id.signin_customer_type);
+        sellerUserType = (RadioButton)findViewById(R.id.signin_seller_type);
+        username_editText = (EditText)findViewById(R.id.signin_username_editText);
+        tellNumber_editText = (EditText)findViewById(R.id.signin_tellNumber_editText);
+        password_editText = (EditText)findViewById(R.id.signin_password_editText);
+        passwordRV_editText = (EditText)findViewById(R.id.signin_passwordReview_editText);
+        tip_username = (TextView)findViewById(R.id.tip_username_textView);
+        tip_password = (TextView)findViewById(R.id.tip_Password_textView);
+        tip_passwordRV = (TextView)findViewById(R.id.tip_PasswordReview_textView);
+        hasId_textView = (TextView)findViewById(R.id.signin_hadId_textView);
+        signIn_button = (Button)findViewById(R.id.signin_button);
+        msgIsLegal = false; //  false as default
+
+
+        setListenerOfWidget();
+    }
+
+    private void setListenerOfWidget(){
+        username_editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                    tip_username.setVisibility(View.VISIBLE);
+                else
+                    tip_username.setVisibility(View.INVISIBLE);
+            }
+        });
+        password_editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                    tip_password.setVisibility(View.VISIBLE);
+                else
+                    tip_password.setVisibility(View.INVISIBLE);
+            }
+        });
+        passwordRV_editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                    tip_passwordRV.setVisibility(View.VISIBLE);
+                else
+                    tip_passwordRV.setVisibility(View.INVISIBLE);
+            }
+        });
+        signIn_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = username_editText.getText().toString();
+                String tellNumber = tellNumber_editText.getText().toString();
+                String password = password_editText.getText().toString();
+                String passwordRV = passwordRV_editText.getText().toString();
+                if("".equals(username)||"".equals(tellNumber)||"".equals(password)||"".equals(passwordRV)){
+                    Toast.makeText(v.getContext(), "输入不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(username.length()<2||username.length()>16||password.length()<8||password.length()>16){
+                    Toast.makeText(v.getContext(), "输入长度不正确，请重新输入", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!password.equals(passwordRV)){
+                    Toast.makeText(v.getContext(), "两次密码不一致，请重新输入", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(tellNumber.matches("^((13[0-9])|(14[5,7,9])|(15[^4])|(18[0-9])|(17[0,1,3,5,6,7,8]))\\\\d{8}$")){
+                    Toast.makeText(v.getContext(), "联系号码输入不正确，请重新输入", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!username.matches("^[(?!_)(?!.*?_$)[a-zA-Z0-9_\\u4e00-\\u9fa5]]{2,16}$")){
+                    Toast.makeText(v.getContext(), "用户名输入有误，请重新输入", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!password.matches("^[a-zA-Z0-9_@#]{8,16}$")){
+                    Toast.makeText(v.getContext(), "密码输入不规范，请重新输入", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                msgIsLegal = true;
+                Toast.makeText(v.getContext(), "MsgLegal", Toast.LENGTH_SHORT).show();
+            }
+        });
+        hasId_textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn_Activity.this, LogIn_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
